10bd8d68630a1e7bd238dbcaaf91ad160ea5ac9f
fh21 <fh21@hw.ac.uk>
2021-03-02 20:03:42 +0000

    test commit

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 // woops we had keys in here
 
-const dbhost="mongodb+srv://dbadmin:examplepassword@cluster0.hx9ge.mongodb.net/test";
+const dbhost =
+  "mongodb+srv://dbadmin:examplepassword@cluster0.hx9ge.mongodb.net/test";
+
+const aws_access_key_id = "AKIAXYZDQCEN24JCQAXD";
