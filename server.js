2b4d0fcf9cf4c6bb8d855a6febc215d8327da626
fh21 <fh21@hw.ac.uk>
2021-03-02 13:13:51 +0000

    removed keys

--- server.js
+++ server.js
@@ -1,5 +1,1 @@
-aws_access_key_id = "AKIAXYZDQCEN24JCQAXD";
-
-// Slack API key
-slack_api_key =
-  "xoxp-905439787527-905447633015-1780433036182-79d35c9a22e449d9df4a93601c0f8fbf";
+// woops we had keys in here
